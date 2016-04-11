864aec51c307e9f6cb22ce0015b2631d587ac07f
Lucas <anycolorbutblack@gmail.com>
2016-04-11 10:54:23 -0400

    Fixes

--- LungLunge-master/LungLunge-master/Assets/scripts/Pedestrian.cs
+++ LungLunge-master/LungLunge-master/Assets/scripts/Pedestrian.cs
@@ -10,12 +10,19 @@ public class Pedestrian : MonoBehaviour {
 	private float Heading;
 	private Vector3 CurrentLocation;
 	Rigidbody rb = null; 
+	public Transform Ped= null;
 	public void Avoid(Vector3 otherped){
 		// should use grid probably
 		//needs animations
 		Vector3 Heading = otherped - transform.position;
-		float dir = Mathf.Sign (Heading.z);
-		Vector3 changer = new Vector3 (transform.position.x, transform.position.y, transform.position.z - dir* 1.5f);
+		Vector3 changer;
+		if (Mathf.Abs(transform.position.x - Destination.x) > 1.0f)  {
+			 float dir = Mathf.Sign (Heading.z);
+			changer = new Vector3 (transform.position.x, transform.position.y, transform.position.z- dir * 1.0f);
+		} else {
+			float dir = Mathf.Sign (Heading.x);
+			changer = new Vector3 (transform.position.x - dir * 1.0f, transform.position.y, transform.position.z);
+		}
 		transform.position = changer;
 		//do animation
 	}
@@ -23,7 +30,7 @@ public class Pedestrian : MonoBehaviour {
 		return CurrentLocation;
 	}
 	public bool DestReached(){
-		if (CurrentLocation == Destination){
+		if (Mathf.Abs(transform.position.x - Destination.x) < 1.0f && Mathf.Abs(transform.position.z - Destination.z) < 1.0){
 			return true;
 		}
 		return false;
@@ -33,17 +40,17 @@ public class Pedestrian : MonoBehaviour {
 		float Xdir = Mathf.Sign(Heading.x);
 		float Zdir = Mathf.Sign(Heading.z);
 		//needs to follow pathing (which doesn't exist at the moment.)
-		if (transform.position.x <= Mathf.Abs(transform.position.x - Destination.x)) {
+		if (Mathf.Abs(transform.position.x - Destination.x) > 1.0f) {
+			transform.forward = Vector3.Normalize (new Vector3 (Xdir, 0f, 0f));
 			this.rb.velocity = new Vector3 (Xdir * Runspeed, rb.velocity.y, rb.velocity.z);
-			transform.rotation = new Quaternion (0, 90*Xdir, 0,1 );
 		} else {
+			transform.forward = Vector3.Normalize (new Vector3 (0f, 0f, Zdir));
 			this.rb.velocity = new Vector3 ( rb.velocity.x, rb.velocity.y, Zdir * Runspeed);
-			transform.rotation = new Quaternion (0, -180*Zdir, 0,1 );
 		}
-	
+
 		//calc path
 		//move toward dest
-				
+
 	}
 	// Use this for initialization
 	void Start () {
@@ -51,19 +58,23 @@ public class Pedestrian : MonoBehaviour {
 		rb = GetComponent<Rigidbody>();
 		//set dest
 		//set spawn pos
+		SpawnPosition = new Vector3 (Random.Range (0f, 50f), 0f, Random.Range (0f, 50f));
+		Destination = new Vector3 (Random.Range (0f, 50f), 0f, Random.Range (0f, 50f));
+
 		if (Destination == SpawnPosition) {
 			Start();
 		}
-		CurrentLocation = SpawnPosition;	
+		transform.position = SpawnPosition;	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		actionController.Walk ();
 		UpdateMovement();
 		if (DestReached() ==true) {
+			Start();
 		}
-			//call despawn
+		//call despawn
 
 	}
 	void OnTriggerEnter(Collider other){
